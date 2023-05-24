191e31858b1fb5b7569ef42b42a555fc5b3bb0dc
Sujoy Paul <freelancersujoy@gmail.com>
2023-05-24 16:56:53 +0600

    socket io simple message test complete

--- backend/index.js
+++ backend/index.js
@@ -14,9 +14,18 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: 'http://localhost:3000/',
+        origin: 'http://localhost:3000',
         methods: ["GET", "POST"]
     }
+})
+
+io.on("connection", (socket) => {
+    console.log(`user id: ${socket.id}`)
+
+    socket.on("send_message", (data) => {
+        socket.broadcast.emit("recive_message", data)
+    })
+
 })
 
 app.get('/', (req, res) => {
--- client-side/src/App.js
+++ client-side/src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import io from 'socket.io-client'
+import {useEffect, useState} from 'react'
+const socket = io.connect("http://localhost:3001")
 
 function App() {
+
+  const [getMsg, setGetMsg] = useState()
+  const [reciveMsg, setReciveMsg] = useState("")
+
+  const handleSendMessage = (event) => {
+    socket.emit("send_message", {message: getMsg})
+  }
+
+  useEffect(() => {
+      socket.on("recive_message", (data) => {
+        // alert(data.message)
+        setReciveMsg(data.message)
+      })
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{reciveMsg}</h1>
+      <input onChange={(e) => setGetMsg(e.target.value)} type="text" placeholder="type your message" />
+      <button onClick={handleSendMessage} className="">Send</button>
     </div>
   );
 }
